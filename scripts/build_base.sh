31eecc6c1aecf63f9c794e7ee4994e5b0e46c281
Tim <niertim308@gmail.com>
2026-02-08 14:07:00 +0100

    Add flag

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -8,6 +8,6 @@ rsync -rvK /ctx/system_files/ /
 ### Install packages
 # dnf5 install -y --setopt=install_weak_deps=0 cloud-init  && dnf clean all
 
-dnf5 install -y --setopt=install_weak_deps=0 cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux firewalld git lm_sensors nfs-utils nss-mdns  pcp pcp-selinux samba sysstat tuned wget && dnf clean all
+dnf5 install -y --setopt=install_weak_deps=0 --skip-unavailable cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux firewalld git lm_sensors nfs-utils nss-mdns  pcp pcp-selinux samba sysstat tuned wget && dnf clean all
 
 systemctl enable lm_sensors sysstat tuned fstrim.timer caddy-build.timer podman-auto-update.timer cockpit.socket
