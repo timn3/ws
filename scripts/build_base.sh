6da79f8f3aa1517c9642f639564b5766d9fb1b50
Tim <niertim308@gmail.com>
2026-02-08 14:10:08 +0100

    Remove unused timer

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -10,4 +10,4 @@ rsync -rvK /ctx/system_files/ /
 
 dnf5 install -y --setopt=install_weak_deps=0 --skip-unavailable cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux firewalld git lm_sensors nfs-utils nss-mdns  pcp pcp-selinux samba sysstat tuned wget && dnf clean all
 
-systemctl enable lm_sensors sysstat tuned fstrim.timer caddy-build.timer podman-auto-update.timer cockpit.socket
+systemctl enable lm_sensors sysstat tuned fstrim.timer podman-auto-update.timer cockpit.socket
