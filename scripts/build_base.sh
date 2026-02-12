bbd44062ce02b1797c466d6c30c7d2120ad07c0c
Tim <niertim308@gmail.com>
2026-02-12 12:56:44 +0100

    Added missing call

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -11,3 +11,6 @@ rsync -rvK /ctx/system_files/ /
 dnf5 install -y --setopt=install_weak_deps=0 --skip-unavailable cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux fastfetch firewalld git lm_sensors nfs-utils nss-mdns  pcp pcp-selinux samba sysstat tuned wget && dnf clean all
 
 systemctl enable lm_sensors sysstat tuned fstrim.timer podman-auto-update.timer cockpit.socket
+
+### Install netbird
+sh /ctx/scripts/install_scripts/install-netbird.sh
