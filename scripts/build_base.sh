fac3866a9bd7571ed81d6d41c47f9b98d965c93e
Tim <niertim308@gmail.com>
2026-02-12 13:57:47 +0100

    Add dependency

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -8,7 +8,7 @@ rsync -rvK /ctx/system_files/ /
 ### Install packages
 # dnf5 install -y --setopt=install_weak_deps=0 cloud-init  && dnf clean all
 
-dnf5 install -y --setopt=install_weak_deps=0 --skip-unavailable cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux fastfetch firewalld git lm_sensors nfs-utils nss-mdns  pcp pcp-selinux samba sysstat tuned wget && dnf clean all
+dnf5 install -y --setopt=install_weak_deps=0 --skip-unavailable cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux dnf5-plugins fastfetch firewalld git lm_sensors nfs-utils nss-mdns  pcp pcp-selinux samba sysstat tuned wget && dnf clean all
 
 systemctl enable lm_sensors sysstat tuned fstrim.timer podman-auto-update.timer cockpit.socket
 
