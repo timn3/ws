a25e09049f40b6d06d39e24bbde97be15188f974
Tim <niertim308@gmail.com>
2026-02-25 21:36:51 +0100

    add rpmfusion  mirrors

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -6,6 +6,7 @@ dnf5 install -y --setopt=install_weak_deps=0 rsync && dnf clean all
 rsync -rvK /ctx/system_files/ /
 
 ### Install packages
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # dnf5 install -y --setopt=install_weak_deps=0 cloud-init  && dnf clean all
 
 dnf5 install -y --setopt=install_weak_deps=0 --skip-unavailable cockpit cockpit-podman cockpit-storaged cockpit-ws cockpit-pcp cockpit-machines cockpit-selinux dnf5-plugins docker docker-compose fastfetch firewalld git lm_sensors nfs-utils nss-mdns pcp pcp-selinux podman podman-compose samba sscg sysstat tuned wget && dnf clean all
