6639ef62054db179560eebd33809b112e7d337f3
Tim <niertim308@gmail.com>
2026-02-28 21:10:16 +0100

    Build initramfs to fix boot issue

--- scripts/build_programs.sh
+++ scripts/build_programs.sh
@@ -55,3 +55,9 @@ systemctl enable lm_sensors sysstat tuned fstrim.timer podman-auto-update.timer 
 sh /ctx/scripts/install_scripts/install-netbird.sh
 
 
+### Build initramfs
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+export DRACUT_NO_XATTR=1
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
