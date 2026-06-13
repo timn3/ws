4227e01277daca9bd6828b7e0ee65ea9611fd598
Tim <niertim308@gmail.com>
2026-06-14 01:30:20 +0200

    added dnf version

--- scripts/build_nvidia.sh
+++ scripts/build_nvidia.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 set -ouex pipefail
+dnf5 config-manager -h
+dnf5 install -y 'dnf5-command(config-manager)'
 
-dnf -y install \
+dnf5 -y install \
     gcc-c++ \
     nvidia-driver \
     dnf-plugins-core \
-    dkms \
-    'dnf-command(config-manager)'
+    dkms
+    
 
 dkms autoinstall -k $(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}') 
-dnf config-manager addrepo --from-repofile https://developer.download.nvidia.com/compute/cuda/repos/fedora43/x86_64/cuda-fedora43.repo
-dnf install -y nvidia-container-toolkit 
-dnf install -y cuda cuda-toolkit cuda-drivers
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
+dnf5 config-manager addrepo --from-repofile https://developer.download.nvidia.com/compute/cuda/repos/fedora43/x86_64/cuda-fedora43.repo
+dnf5 install -y nvidia-container-toolkit 
+dnf5 install -y cuda cuda-toolkit cuda-drivers
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
 
-dnf install -y nvidia-vaapi-driver libva-nvidia-driver libva-utils
+dnf5 install -y nvidia-vaapi-driver libva-nvidia-driver libva-utils
 
 akmods --force --kernels "$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-devel)" 
 
-dnf clean all
+dnf5 clean all
 
 tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<EOF
 kargs = ["nvidia-drm.modeset=1 nouveau.modeset=0 rd.driver.blacklist=nouveau,nova-core modprobe.blacklist=nouveau,nova-core", "initcall_blacklist=simpledrm_platform_driver_init"]
