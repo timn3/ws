4c76993c587c9e0d097ea2498f5fe6e7dedaada7
Tim <niertim308@gmail.com>
2026-06-14 13:36:35 +0200

    not yet available package

--- scripts/build_nvidia.sh
+++ scripts/build_nvidia.sh
@@ -18,7 +18,7 @@ dnf5 install -y --allowerasing --skip-broken \
     nvidia-container-toolkit 
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
 
-dnf5 install -y \
+dnf5 install -y --skip-unavailable \
     nvidia-vaapi-driver \
     libva-nvidia-driver \
     libva-utils
